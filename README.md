ba29e9ec0481ecd5a017415a2652ce7c2d8ae1f0
Zois Pagoulatos <zpagoulatos@hotmail.com>
2020-04-14 21:36:23 +0200

    Initial commit 🚀

---
+++ README.md
@@ -0,0 +1,3 @@
+### HTTP to HTTPS Proxy for energymixradio.com, written in PHP.
+
+Kudos to [jenssegers/php-proxy](https://github.com/jenssegers/php-proxy)
---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+
+require __DIR__ . '/vendor/autoload.php';
+
+use Proxy\Proxy;
+use Proxy\Adapter\Guzzle\GuzzleAdapter;
+use Proxy\Filter\RemoveEncodingFilter;
+use Laminas\Diactoros\ServerRequestFactory;
+
+// Create a PSR7 request based on the current browser request.
+$request = ServerRequestFactory::fromGlobals();
+
+// Create a guzzle client
+$guzzle = new GuzzleHttp\Client();
+
+// Create the proxy instance
+$proxy = new Proxy(new GuzzleAdapter($guzzle));
+
+// Add a response filter that removes the encoding headers.
+$proxy->filter(new RemoveEncodingFilter());
+
+// Forward the request and get the response.
+$response = $proxy->forward($request)->to('http://cast.magicstreams.gr:9117/;stream.mp3');
+
+// Output response to the browser.
+(new Laminas\HttpHandlerRunner\Emitter\SapiEmitter)->emit($response);
